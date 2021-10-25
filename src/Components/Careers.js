153326f53864491833908bacabed4f21fc64f199
Yutuc <psyutuc@hotmail.com>
2021-10-25 18:42:05 -0500

    Added UI for the job application form

--- src/Components/Careers.js
+++ src/Components/Careers.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import Fade from "react-reveal";
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 
 class About extends Component {
   render() {
@@ -16,6 +24,26 @@ class About extends Component {
     const email = this.props.data.email;
     const resumeDownload = this.props.data.resumedownload;
 
+    const useStyles = makeStyles((theme) => ({
+        finish: {
+          padding: theme.spacing(.75),
+          width: '100%',
+          height: '100%',
+          maxWidth: '100%',
+          maxHeight: '100%',
+          backgroundColor: "#07080fdc",
+          borderRadius: "10%",
+          fontWeight: "700",
+        },
+    }));
+
+    const styles = {
+        size: {
+          width: 40,
+          height: 40,
+        },
+      };
+
     return (
       <section id="careers">
         <Fade duration={1000}>
@@ -28,27 +56,91 @@ class About extends Component {
               />
             </div>
             <div className="nine columns main-col">
-              <h2>Careers</h2>
-
-              <p>{bio}</p>
-              <div className="row">
-                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    <span>{name}</span>
-                    <br />
-                    <span>
-                      {street}
-                      <br />
-                      {city} {state}, {zip}
-                    </span>
-                    <br />
-                    <span>{phone}</span>
-                    <br />
-                    <span>{email}</span>
-                  </p>
-                </div>
-              </div>
+              <h2 class="form-headers">Complete the form below to apply for a position with us</h2>
+              <Grid container direction="row" justifyContent="center">
+                    <Grid item xs={12}>
+                        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', flexDirection:'column'}}>
+                            <form id="input-box" noValidate autoComplete="off" /*onSubmit={handleSubmit}*/>
+                                <TextField onChange={(event) => {
+                                // props.onChangeName(event.target.value)
+                                }} id="outlined-basic" style={{textAlign: 'left'}} label={<Typography id="typography">First name</Typography>} variant="outlined" inputProps={{min: 0, style: { textAlign: 'left', paddingTop: 30, fontSize: 20}}} />
+                            </form>
+                            <form id="input-box" noValidate autoComplete="off" /*onSubmit={handleSubmit}*/>
+                            <TextField onChange={(event) => {
+                                // props.onChangePhone(event.target.value)
+                                }} id="outlined-basic" style={{textAlign: 'left'}} label={<Typography id="typography">Last name</Typography>} variant="outlined" inputProps={{min: 0, style: { textAlign: 'left', paddingTop: 30, fontSize: 20}}} />
+                            </form>
+                            <form id="input-box" noValidate autoComplete="off" /*onSubmit={handleSubmit}*/>
+                            <TextField onChange={(event) => {
+                                // props.onChangePhone(event.target.value)
+                                }} id="outlined-basic" style={{textAlign: 'left'}} label={<Typography id="typography">Email</Typography>} variant="outlined" inputProps={{min: 0, style: { textAlign: 'left', paddingTop: 30, fontSize: 20}}} />
+                            </form>
+                            <form id="input-box" noValidate autoComplete="off" /*onSubmit={handleSubmit}*/>
+                            <TextField onChange={(event) => {
+                                // props.onChangePhone(event.target.value)
+                                }} id="outlined-basic" style={{textAlign: 'left'}} label={<Typography id="typography">Phone number</Typography>} variant="outlined" inputProps={{min: 0, style: { textAlign: 'left', paddingTop: 30, fontSize: 20}}} />
+                            </form>
+                            <form id="input-box" noValidate autoComplete="off" /*onSubmit={handleSubmit}*/>
+                            <TextField onChange={(event) => {
+                                // props.onChangePhone(event.target.value)
+                                }} id="outlined-basic" style={{textAlign: 'left'}} label={<Typography id="typography">When can you start? (MM/DD/YY)</Typography>} variant="outlined" inputProps={{min: 0, style: { textAlign: 'left', paddingTop: 30, fontSize: 20}}} />
+                            </form>
+                        </div>
+                    </Grid>
+                </Grid>
+                <h2 class="form-headers">Position</h2>
+                <Grid container direction="row" justifyContent="center">
+                    <Grid item xs={4} style={{paddingLeft:"30px"}}>
+                        <FormControlLabel
+                            control={ <Checkbox className={styles.size} /*onChange={props.handleCheckboxes} value={props.answerContent} name={props.answerContent}*//> }
+                            label={<Typography style={{fontSize: 20, color: "black"}}>Cleaner</Typography>}
+                        />
+                    </Grid>
+                    <Grid item xs={4} style={{paddingLeft:"30px"}}>
+                        <FormControlLabel
+                            control={ <Checkbox className={styles.size} /*onChange={props.handleCheckboxes} value={props.answerContent} name={props.answerContent}*//> }
+                            label={<Typography style={{fontSize: 20, color: "black"}}>Site Supervisor</Typography>}
+                        />
+                    </Grid>
+                    <Grid item xs={4} style={{paddingLeft:"30px"}}>
+                    </Grid>
+                </Grid>
+                <h2 class="form-headers">Availability</h2>
+                <Grid container direction="row" justifyContent="center">
+                    <Grid item xs={4} style={{paddingLeft:"30px"}}>
+                        <FormControlLabel
+                            control={ <Checkbox className={styles.size} /*onChange={props.handleCheckboxes} value={props.answerContent} name={props.answerContent}*//> }
+                            label={<Typography style={{fontSize: 20, color: "black"}}>Days</Typography>}
+                        />
+                    </Grid>
+                    <Grid item xs={4} style={{paddingLeft:"30px"}}>
+                        <FormControlLabel
+                            control={ <Checkbox className={styles.size} /*onChange={props.handleCheckboxes} value={props.answerContent} name={props.answerContent}*//> }
+                            label={<Typography style={{fontSize: 20, color: "black"}}>Evenings</Typography>}
+                        />
+                    </Grid>
+                    <Grid item xs={4} style={{paddingLeft:"30px"}}>
+                        <FormControlLabel
+                            control={ <Checkbox className={styles.size} /*onChange={props.handleCheckboxes} value={props.answerContent} name={props.answerContent}*//> }
+                            label={<Typography style={{fontSize: 20, color: "black"}}>Nights</Typography>}
+                        />
+                    </Grid>
+                </Grid>
+                <Button style={{textTransform: 'none', float: 'right'}} onClick={() => {
+                        // if(nameTextFieldValue !== "" && phoneTextFieldValue !== "" && emailTextFieldValue !== "") {
+                        //     props.onAnswerSelected(props.question, 
+                        //     {
+                        //     name: nameTextFieldValue,
+                        //     phone: phoneTextFieldValue,
+                        //     email: emailTextFieldValue
+                        //     }, props.answerOptions[0].type);
+                        //     nameTextFieldValue = "";
+                        //     phoneTextFieldValue = "";
+                        //     emailTextFieldValue = "";
+                        // }
+                        }}>
+                        <Paper className={useStyles.finish} style={{fontSize: 28, color: 'white', backgroundColor: "#07080fdc", padding: .75, borderRadius: "7%", fontWeight: "700"}}>&#160;&#160;&#160;Finish&#160;&#160;&#160;</Paper>
+                </Button>
             </div>
           </div>
         </Fade>
